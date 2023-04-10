0b39a7aa12a1a4e81ac5c44dda12c81009cc9eb7
Brett Lamy <brettlamy@gmail.com>
2023-04-10 11:19:49 -0700

    Can you add a rule that says anyone can access pub

--- src/ai/generated/Rules.ts
+++ src/ai/generated/Rules.ts
@@ -1,12 +1,1 @@
-export default {
-	"rules": [{
-		"role": "admin",
-		"access": "all"
-	}, {
-		"role": "employee",
-		"access": "non_confidential"
-	}, {
-		"username": "BillyBob",
-		"access": "confidential"
-	}]
-} as const;
+export default \n{\"rules\": [{\"role\": \"admin\", \"access\": \"all\"}, {\"role\": \"employee\", \"access\": \"non_confidential\"}, {\"username\": \"BillyBob\", \"access\": \"confidential\"}, {\"access\": \"public\", \"ban\": [\"Mike\"]}]} as const;
