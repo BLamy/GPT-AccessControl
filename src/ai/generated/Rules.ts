f26d0961ace99d1174941fc41113cd48c7e1ae4e
Brett Lamy <brettlamy@gmail.com>
2023-04-10 13:13:15 -0700

    Can you add a rule that says anyone can perform pu

--- src/ai/generated/Rules.ts
+++ src/ai/generated/Rules.ts
@@ -1,12 +1,27 @@
 export default {
-	"rules": [{
-		"role": "admin",
-		"access": "all"
-	}, {
-		"role": "employee",
-		"access": "non_confidential"
-	}, {
-		"username": "BillyBob",
-		"access": "confidential"
-	}]
+  "rules": [
+    {
+      "role": "admin",
+      "access": "all"
+    },
+    {
+      "role": "employee",
+      "access": "non_confidential"
+    },
+    {
+      "username": "BillyBob",
+      "access": "confidential"
+    },
+    {
+      "ban_list": [
+        "Mike"
+      ]
+    },
+    {
+      "access": "public",
+      "ban_list": [
+        "Mike"
+      ]
+    }
+  ]
 } as const;
