03300fb65533f9b4f3496c2ce602897ca9c8c443
Brett Lamy <brettlamy@gmail.com>
2023-05-02 16:32:48 -0700

    {"rules": [{"role": "admin", "access": "all"}

--- src/ai/generated/Rules.ts
+++ src/ai/generated/Rules.ts
@@ -1,12 +1,16 @@
 export default {
-	"rules": [{
-		"role": "admin",
-		"access": "all"
-	}, {
-		"role": "employee",
-		"access": "non_confidential"
-	}, {
-		"username": "BillyBob",
-		"access": "confidential"
-	}]
+  "rules": [
+    {
+      "role": "admin",
+      "access": "all"
+    },
+    {
+      "role": "employee",
+      "access": "non_confidential"
+    },
+    {
+      "username": "BillyBob",
+      "access": "confidential"
+    }
+  ]
 } as const;
